311fa01f5b2f18122c914b6a011298e94518a704
jinlow <james.d.inlow@gmail.com>
2022-11-13 15:28:33 -0600

    Adding short brown-noise for testing

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     // in the sound machine, these will be precompilfed into the
     // actual binary, to make it more portable.
     //let mut sounds: HashMap<String, &[u8;0]> = HashMap::new();
-    let sounds: [&[u8]; 3] = [
+    let sounds: [&[u8]; 4] = [
         std::include_bytes!("../resources/gentle-white-noise.mp3"),
         std::include_bytes!("../resources/brown-noise-seamless.mp3"),
+        std::include_bytes!("../resources/brown-noise-seamless-short.mp3"),
         std::include_bytes!("../resources/white-noise-short.mp3"),
     ];
     let sink = Sink::try_new(&stream_handle).unwrap();
-    let cursor = Cursor::new(sounds[1]);
+    let cursor = Cursor::new(sounds[2]);
     let source = Decoder::new(cursor).unwrap();
     sink.append(source.repeat_infinite());
     sink.sleep_until_end();
