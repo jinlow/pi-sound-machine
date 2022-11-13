d6d1c7909ad86dcaf2aabdacdddb37017759a82a
jinlow <james.d.inlow@gmail.com>
2022-11-13 15:15:26 -0600

    Adding short noise, making an inifite run

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::fs::File;
-use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source, Sink};
+use rodio::{Decoder, OutputStream, Sink, Source};
+use std::error::Error;
+use std::io::Cursor;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open("resources/brown-noise-seamless.mp3")
-    .expect("Unable to read file."));
-    let sink = Sink::try_new(&stream_handle).unwrap();
 
-    let source = Decoder::new(file).unwrap();
-    sink.append(source);
+    // Create an array of all the sounds we want included
+    // in the sound machine, these will be precompilfed into the
+    // actual binary, to make it more portable.
+    //let mut sounds: HashMap<String, &[u8;0]> = HashMap::new();
+    let sounds: [&[u8]; 3] = [
+        std::include_bytes!("../resources/gentle-white-noise.mp3"),
+        std::include_bytes!("../resources/brown-noise-seamless.mp3"),
+        std::include_bytes!("../resources/white-noise-short.mp3"),
+    ];
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    let cursor = Cursor::new(sounds[1]);
+    let source = Decoder::new(cursor).unwrap();
+    sink.append(source.repeat_infinite());
     sink.sleep_until_end();
+    Ok(())
 }
