f3f1ead51b3a751eb4c9815fd9a2ba443e2d4ad2
jinlow <james.d.inlow@gmail.com>
2022-11-15 15:04:56 -0600

    Added low pass and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
 mod whitenoise;
 
-use rodio::{Decoder, OutputStream, Sink, Source, source::SineWave};
-use std::time::Duration;
+use rodio::{OutputStream, Sink, Source};
 use std::error::Error;
-use std::io::Cursor;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    // Create an array of all the sounds we want included
-    // in the sound machine, these will be precompilfed into the
-    // actual binary, to make it more portable.
-    //let mut sounds: HashMap<String, &[u8;0]> = HashMap::new();
-    let sounds: [&[u8]; 4] = [
-        std::include_bytes!("../resources/gentle-white-noise.mp3"),
-        std::include_bytes!("../resources/brown-noise-seamless.mp3"),
-        std::include_bytes!("../resources/brown-noise-seamless-short.mp3"),
-        std::include_bytes!("../resources/white-noise-short.mp3"),
-    ];
     let sink = Sink::try_new(&stream_handle).unwrap();
-    let cursor = Cursor::new(sounds[2]);
-    // let source = Decoder::new(cursor).unwrap();
-    // let source = SineWave::new(100.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20);
-    let source = whitenoise::WhiteNoise::new(0.8); //.amplify(100.0);
+    let source = whitenoise::WhiteNoise::new(0.5).low_pass(500);
+    
     // We want the sound to go forever, however, I need to figure
     // out how to make the fade smoother.
     sink.append(source.repeat_infinite());
--- src/whitenoise.rs
+++ src/whitenoise.rs
@@ -1,12 +1,15 @@
 use rodio::source::Source;
-
 // Generate random value between -1.0, and 1.0.
 fn rand_value() -> f32 {
     (fastrand::f32() - 0.5) * 2.0
 }
 
+/// White Noise generator
+/// The alpha parameter sets the color, the lower the value, the
+/// darker the color (brown noise), the higher the value, the
+/// brighter the color (white to violet noise).
+#[derive(Clone)]
 pub struct WhiteNoise {
-    //rng: ThreadRng
     value: f32,
     alpha: f32,
 }
@@ -15,7 +18,7 @@ impl WhiteNoise {
     #[inline]
     pub fn new(alpha: f32) -> Self {
         WhiteNoise {
-            value: rand_value(),
+            value: 0.0,
             alpha,
         }
     }
@@ -26,8 +29,19 @@ impl Iterator for WhiteNoise {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
+        // Adding this clamp, instead of using the sine functions, seemed to sound a little better.
+        if (self.value > 1.0) || (self.value < -1.0) {
+            self.value *= self.alpha
+        }
+
+        // If the previous filter didn't make it small enough, clip back to zero
+        if self.value > 1.0 {
+            self.value = 0.0
+        } else if self.value < -1.0 {
+            self.value = 0.0
+        }
         self.value += rand_value() * self.alpha;
-        Some(self.value.sin())
+        Some(self.value)
     }
 }
 
