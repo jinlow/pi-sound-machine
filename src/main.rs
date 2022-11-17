f4978b15e8da457b135a1264345d4dd9047ee048
jinlow <james.d.inlow@gmail.com>
2022-11-17 12:57:24 -0600

    Fix memory leak

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     let sink = Sink::try_new(&stream_handle).unwrap();
-    let source = whitenoise::WhiteNoise::new(0.5).low_pass(500);
-    
+    let source = whitenoise::WhiteNoise::new(0.2)
+        .take_duration(std::time::Duration::from_secs(30))
+        .low_pass(500);
+
     // We want the sound to go forever, however, I need to figure
     // out how to make the fade smoother.
     sink.append(source.repeat_infinite());
